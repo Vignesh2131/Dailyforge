2c98cd282b2b128e3f057e5a38420235e5c36213
Vignesh2131 <vigneshsadhu143@gmail.com>
2025-01-02 21:54:06 +0530

    intial setup

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+  return (
+    <div>
+      <Button>
+        Shad cn comp button
+      </Button>
+   </div>
+  )
+}
+
+export default App
