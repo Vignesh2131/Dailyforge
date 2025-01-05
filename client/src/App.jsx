1eb614087b11074a7da6d255697dee767268ce02
Vignesh2131 <vigneshsadhu143@gmail.com>
2025-01-05 22:57:58 +0530

    frontend file config

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,19 @@
-import { Button } from "./components/ui/button"
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import Signin from "./pages/auth/Signin";
+import Signup from "./pages/auth/Signup";
+import Home from "./pages/Home";
 function App() {
   return (
     <div>
-      <Button>
-        Shad cn comp button
-      </Button>
-   </div>
-  )
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App
