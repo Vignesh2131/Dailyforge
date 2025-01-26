968acfd16af0658c8a40ce3b59074415a64a80df
Vignesh2131 <vigneshsadhu143@gmail.com>
2025-01-26 23:16:09 +0530

    config changes

--- server/index.js
+++ server/index.js
@@ -12,8 +12,7 @@ const db = require("./db/db");
 db();
 app.use(cookieParser())
 app.use(express.json());
-app.use(cors({ credentials: true, origin: "http://localhost:5173" , httpOnly: true,
-  sameSite: "None", }));
+app.use(cors({ credentials: true, origin: "http://localhost:5173"}));
 
 app.use("/v1",authMiddleware,userRouter)
 app.use("/auth", authRouter);
